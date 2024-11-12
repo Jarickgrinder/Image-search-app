f8f33400c51e481f05203bde097a5a51deee75d3
Jarickgrinder <166217017+Jarickgrinder@users.noreply.github.com>
2024-11-12 02:21:57 +0200

    Update ImageCard.js

--- src/app/js/ImageCard.js
+++ src/app/js/ImageCard.js
@@ -11,44 +11,4 @@ const ImageCard = ({ image }) => {
 };
 
 
-
-
-// let galleryImages = document.querySelectorAll(".img");
-// let getLatestOpendImg;
-// let windowWidth = window.innerWidth;
-
-// if(galleryImages) {
-//     galleryImages.forEach(function(image, index) {
-//         image.onclick = function() {
-//             // let getElementCss = window.getComputedStyle(image);
-//             // let getFullImgUrl = getElementCss.getPropertyValue("background-image")
-//             // let getImgUrlPos = getFullImgUrl.split("/img/thumbs/")
-//             let setNewImgUrl = image.webformatURL
-//             // getImgUrlPos[1].replace('")', '');
-//             console.log(image)
-
-//             // getLatestOpendImg = index + 1;
-
-//             let container = document.body;
-//             let newImgWindow = document.createElement("div");
-//             container.appendChild(newImgWindow);
-//             newImgWindow.setAttribute("class", "img-window");
-//             newImgWindow.setAttribute("onclick", "closeImg()")
-
-//             let newImg = document.createElement("img");
-//             newImgWindow.appendChild(newImg);
-//             newImg.setAttribute("src", + image);    
-             
-//             console.log(setNewImgUrl)
-//             console.log(newImgWindow)
-//             console.log(newImg) 
- 
-//         }  
-//     });     
-// }
- 
-// function closeImg () {
-//     document.querySelector(".img-window").remove();
-// }
-
 export default ImageCard;
